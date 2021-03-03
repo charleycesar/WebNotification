1f0e7def33a45618b05a47ad195e6c9244c081be
Charley Oliveira <charleycesar@gmail.com>
2021-03-02 23:11:33 -0300

    Delete firebase.config.js

--- src/config/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAFZU0_Fda-ksTu_S_bCY15EltzDy3HsRk',
-  authDomain: 'webnotification-27548.firebaseapp.com',
-  projectId: 'webnotification-27548',
-  storageBucket: 'webnotification-27548.appspot.com',
-  messagingSenderId: '1036684750344',
-  appId: '1:1036684750344:web:6866e724ad01ad22e6abbd',
-};
